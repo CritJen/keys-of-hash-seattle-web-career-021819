ba8c07bbe8f092e63700b60bbb5dc7947308660d
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 23:27:55 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,11 +2,11 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     arr = []
-      hash.each do |key, value|
+      self.each do |key, value|
         binding.pry
         if value == arguments
           arr << key
-        end 
+        end
       end
   end
 end
